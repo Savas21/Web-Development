854e0db6d45c147638adf14d0a0316bdb42476ac
Savas Karakus <savas.kara21@gmail.com>
2018-06-21 17:13:49 -0700

    added a file

---
+++ 04-ORMs,APIs/1_Classes/a_classes0.py
@@ -0,0 +1,11 @@
+#Define class in Python 
+
+#create new python class
+class Flight:
+
+    #create special init mehtod 
+    def __init__(self, origin, destination ,duration):
+        self.origin = origin
+        self.destination = destination
+        self.duration = duration
+    
